ebbb049c95c534ba112b9601aab478e48d6be1a3
Hak0um <80795611+Hak0um@users.noreply.github.com>
2023-06-11 17:33:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// login
+function viewLogInTab() {
+    document.getElementById('id01').style.display = 'block'
+}
+function closeLogInTab() {
+    document.getElementById('id01').style.display = 'none'
+}
+
+// nav
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
